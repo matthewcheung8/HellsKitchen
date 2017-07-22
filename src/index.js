9c52d4653d402bf907d342d891164517bed607ad
Cheung <mccheun@amazon.com>
2017-07-21 17:09:52 -0700

    Fix bugs

--- src/index.js
+++ src/index.js
@@ -1,50 +1,62 @@
 'use strict';
-var ssml = require('ssml');
-var ssmlDoc = new ssml();
+var Alexa = require('alexa-sdk');
 
-const Alexa = require('alexa-sdk');
+//=========================================================================================================================================
+//TODO: The items below this comment need your attention.
+//=========================================================================================================================================
 
-const APP_ID = "amzn1.ask.skill.63f6503c-2034-4c96-879d-9888cebe06e0";
+//Replace with your app ID (OPTIONAL).  You can find this value at the top of your skill's page on http://developer.amazon.com.
+//Make sure to enclose your value in quotes, like this: var APP_ID = "amzn1.ask.skill.bb4045e6-b3e8-4133-b650-72923c5980f1";
+var APP_ID = undefined;
+
 const SKILL_NAME = "Hell's Kitchen";
 const HELP_MESSAGE = "Ask me for motivation or to rate your dish";
 const HELP_REPROMPT = "What can I help you with?";
 const STOP_MESSAGE = "Goodbye!";
-
-const mean_link = "https://s3.amazonaws.com/hells-kitchen-skill/mean/";
-const nice_link = "https://s3.amazonaws.com/hells-kitchen-skill/nice/";
+const MEAN_LINK = "https://s3.amazonaws.com/hells-kitchen-skill/mean/";
+const NICE_LINK = "https://s3.amazonaws.com/hells-kitchen-skill/nice/";
 var mean_files = ["bisons1.mp3","donkey1.mp3","hell1.mp3","hotdog1.mp3",
 "inedible1.mp3","leave1.mp3","omg1.mp3","pig1.mp3","pissoff1.mp3","score1.mp3"];
 var nice_files = ["goodburger1.mp3","gooddelicious1.mp3","goodfavorite1.mp3",
 "goodgirl1.mp3","goodjesus1.mp3","goodnicedelicate1.mp3","goodribs1.mp3","goodseason1.mp3","goodspoton1.mp3"];
-var links = [mean_link, nice_link];
 var files = [mean_files, nice_files];
+var links = [MEAN_LINK, NICE_LINK];
+
+exports.handler = function(event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.APP_ID = APP_ID;
+    alexa.registerHandlers(handlers);
+    alexa.execute();
+};
 
 var handlers = {
     'LaunchRequest': function () {
-        this.emit('GetNewQuoteIntent');
-    },
-    'NiceQuotesIntent': function () {
-        var quoteArr = nice_files;
-        var quoteIndex = Math.floor(Math.random() * quoteArr.length);
-        var randomQuote = quoteArr[quoteIndex];
-        var speechOutput = nice_link.concat(randomQuote);
-        ssmlDoc.audio({src: speechOutput}).toString();
+        this.emit('GetMeanQuote');
     },
     'MeanQuotesIntent': function () {
-        var quoteArr = mean_files;
-        var quoteIndex = Math.floor(Math.random() * quoteArr.length);
-        var randomQuote = quoteArr[quoteIndex];
-        var speechOutput = mean_link.concat(randomQuote);
-        ssmlDoc.audio({src: speechOutput}).toString();
+        var factArr = mean_files;
+        var factIndex = Math.floor(Math.random() * factArr.length);
+        var randomFact = factArr[factIndex];
+        var speechOutput = MEAN_LINK + randomFact;
+        var output = '<audio src=' + "\"" + speechOutput + "\"" + '/>';
+        this.emit(':tell', output);
+    },
+    'NiceQuotesIntent': function () {
+        var factArr = nice_files;
+        var factIndex = Math.floor(Math.random() * factArr.length);
+        var randomFact = factArr[factIndex];
+        var speechOutput = NICE_LINK + randomFact;
+        var output = '<audio src=' + "\"" + speechOutput + "\"" + '/>';
+        this.emit(':tell', output);
     },
     'RandQuotesIntent': function () {
-        var typeIndex = Math.floor(Math.random() * 2);
-        var quoteArr = files[typeIndex];
-        var quoteLink = links[typeIndex];
-        var quoteIndex = Math.floor(Math.random() * quoteArr.length);
-        var randomQuote = quoteArr[quoteIndex];
-        var speechOutput = quoteLink.concat(randomQuote);
-        ssmlDoc.audio({src: speechOutput}).toString();
+        var filesIndex = Math.floor(Math.random() * 2);
+        var factArr = files[filesIndex];
+        var factIndex = Math.floor(Math.random() * factArr.length);
+        var randomFact = factArr[factIndex];
+        var speechOutput = links[filesIndex] + randomFact;
+        var output = '<audio src=' + "\"" + speechOutput + "\"" + '/>';
+        this.emit(':tell', output);
     },
     'AMAZON.HelpIntent': function () {
         var speechOutput = HELP_MESSAGE;
@@ -57,11 +69,4 @@ var handlers = {
     'AMAZON.StopIntent': function () {
         this.emit(':tell', STOP_MESSAGE);
     }
-};
-
-exports.handler = function(event, context, callback) {
-    var alexa = Alexa.handler(event, context);
-    alexa.APP_ID = APP_ID;
-    alexa.registerHandlers(handlers);
-    alexa.execute();
 };
